bc3862d51927729dc41b4a96cf6536f9bc801aa3
Ricky Huang <huangricky@gmail.com>
2019-11-21 22:21:56 -0500

    messin w Svelte

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+	let name = 'Rico';
+	let age = 38;
+
+	$: uppercaseName = name.toUpperCase();
+	$: console.log(name);
+	$: if (name === "Ricardo Baby") {
+		age = 52;
+	}
+
+	function incrementAge() {
+		age++;
+	}
+	function changeName() {
+		name = "Ricardo Baby";
+	}
+	function inputName(event) {
+		const enteredValue = event.target.value;
+		name = enteredValue;
+	}
+</script>
+
+<style>
+	h1 {
+		color: #00CED1;
+	}
+</style>
+
+<h1>Hola {uppercaseName}! You {age}-year old freak!</h1>
+<button on:click="{incrementAge}">Change Age</button>
+<!-- <button on:click="{changeName}">Change Name</button> -->
+<!-- <input type="text" value="{name}" on:input="{inputName}"> -->
+<input type="text" bind:value="{name}">
